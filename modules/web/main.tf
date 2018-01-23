752bf41a2a73ab6e7ec4ae1363f47763f921ceff
Arockiasamy K <Arockiasamy.K@version1.com>
2018-01-23 17:12:18 +0000

    Configured AutoLaunch and AutoScaling.

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -1,14 +1,41 @@
-# Create an EC2 instance
-resource "aws_instance" "web" {
-  # AMI ID for CentOS
-  ami           = "${var.ami}"
-  instance_type = "t2.micro"
+# Create Autolaunch configuration
+resource "aws_launch_configuration" "al-tf-web" {
+  name           = "al-tf-web"
+  instance_type  = "t2.micro"
+  image_id       = "${var.ami}"
+  security_groups = ["${aws_security_group.test-web.id}"]
   key_name      =  "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.test-web.id}"]
-  subnet_id = "${var.subnet_id}"
 
-  tags {
-    Name = "Web",
-    CR = "SHUTDOWN"
+  lifecycle {
+    create_before_destroy = true
   }
+
+}
+
+# Create AutoScaling group
+resource "aws_autoscaling_group" "as-tf-web" {
+  name = "as-tf-web"
+  launch_configuration = "${aws_launch_configuration.al-tf-web.name}"
+  min_size = 1
+  max_size = 2
+  desired_capacity = 1
+  target_group_arns = ["${aws_lb_target_group.tf-tg.arn}"]
+  vpc_zone_identifier = ["${var.subnet_weba}","${var.subnet_webb}"]
+
+  lifecycle {
+  create_before_destroy = true
+  }
+
+  tags = [
+    {
+      key                 = "CR"
+      value               = "SHUTDOWN"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Name"
+      value               = "TF-Web"
+      propagate_at_launch = true
+    },
+  ]
 }
