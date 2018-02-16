94368a7fdc137f7d4b33e1913a19f8c198f04a5d
Arockiasamy K <Arockiasamy.K@version1.com>
2018-02-16 16:38:36 +0000

    Fixed the issue.

---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  description = "Region where the servcies needed to be hosted."
+}
+
+variable "key_name" {
+  description = "Region where the servcies needed to be hosted."
+}
